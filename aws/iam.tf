3fd999602730ed37ec8b21b78df9d64ce4d745a4
Bruce H <bh3791@outlook.com>
2025-02-21 09:29:38 -0800

    Initial commit (extracted folder, no previous history)

---
+++ aws/iam.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "ecs_task_exec_role" {
+  name               = "${var.deployment_name}_task_exec_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_ecs.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy_ecs" {
+  version = "2012-10-17"
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_role" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_role_cw" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_role_s3" {
+  role       = aws_iam_role.ecs_task_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role" "aws_batch_service_role" {
+  name               = "${var.deployment_name}_batch_service_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_batch.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy_batch" {
+  version = "2012-10-17"
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["batch.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_batch_service_role" {
+  role       = aws_iam_role.aws_batch_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
+}
